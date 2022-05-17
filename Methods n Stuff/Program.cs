d6cd8d7c427502ecf1c7d1acdd8e9261d17f04dd
Hurd <104808709+Hurd-IT@users.noreply.github.com>
2022-05-18 10:03:57 +1200

    Adding current projects to repo

---
+++ Methods n Stuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Methods_n_Stuff
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
